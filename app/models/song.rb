82ff665bde88f9587083d3450ded64ca7b61deb6
Pamela Frieze <pfrieze@gmail.com>
2018-10-29 20:45:00 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,9 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
+  def artist_name
+      self.try(:artist).try(:name)
+  end
 
   def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
